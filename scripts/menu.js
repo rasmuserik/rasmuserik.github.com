2546978b5614fed6ee196cb6250775364b7aea28
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2012-02-25 16:05:29 +0100

    hierachy

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,7 +1,10 @@
 define(['jquery'], function($) {
     var arraySlice = Array.prototype.slice.apply.bind(Array.prototype.slice);
+    var margin = 4;
+    var titlesize = 20;
 
     function elemToObj(elem) {
+        var result = {};
         return {
             title: $(elem.firstChild).text().trim(),
             children: arraySlice($(elem).find('> ul > li'))
@@ -11,14 +14,34 @@ define(['jquery'], function($) {
         }
     }
 
+    var seed = 1;
+    function pseudoRandom(n) {
+        return seed = (1664525 * (n || seed) + 1013904223) |0
+    }
+
+    function colorHash(text) {
+        var result = 0;
+        for(var i=0;i<text.length; ++i) {
+            result = pseudoRandom(result + text.charCodeAt(i));
+        }
+        return "#" + ((result | 0xe0e0e0)&0xffffff).toString(16);
+    }
+
     function reset(menu) {
         var style = menu.elem.style;
         style.display = 'block'; 
         style.position = 'absolute'; 
+        style.padding = margin/2 + 'px';
         style.top = '100px'; 
         style.left = '100px'; 
-        style.width = '500px'; 
-        style.border = '1px solid black'; 
+        style.margin = margin + 'px';
+        style.textAlign = 'center';
+        style.borderRadius = margin*2 + 'px';
+        //style.border = '1px solid #888888'; 
+        style.overflow = 'hidden';
+        style.backgroundColor = colorHash(menu.title);
+        style.boxShadow = '3px 3px 9px rgba(0, 0, 0, 0.5)';
+        //style.webkitBoxShadow = 'inset 3px 3px 9px rgba(0, 0, 0, 0.5)';
         menu.children.forEach(reset);
     }
     function totalSize(arr) {
@@ -27,62 +50,81 @@ define(['jquery'], function($) {
 
     function calcSize(menu) {
         menu.children.forEach(calcSize);
-        menu.size = totalSize(menu.children)/3 + 1;
+        menu.size = totalSize(menu.children)/1.5 + 1;
     }
     function position(menu, x, y, w, h) {
-        console.log('position', menu.title, x, y ,w, h);
+        w-= 2*margin;
+        h-= 2*margin;
         var style = menu.elem.style;
         style.left = x + 'px';
         style.top = y + 'px';
         style.width = w + 'px';
         style.height = h + 'px';
-        positionArray(menu.children, 0, 40, w, h-40);
+        positionArray(menu.children, margin, titlesize+margin, w-margin*2, h-titlesize-margin*2);
     }
 
-    blah = 100;
     function positionArray(arr, x, y, w, h) {
-        console.log('positionArray', arr, x, y, w, h);
         if(arr.length === 0) {
             return;
         }
         if(arr.length === 1) {
             return position(arr[0], x, y, w, h);
         }
-        var size = totalSize(arr);
-        var sizeSum = 0;
+        var weight = totalSize(arr);
+        var splitsum = weight;
+        var sum = 0;
 
         // find the position in the array which
         // which splits it in two of same weight
-        var pos = -1;
-        while(sizeSum < size/2) {
-            sizeSum += arr[++pos].size;
-        }
-        if(size/2-(sizeSum-arr[pos].size) > sizeSum - size/2) {
-            ++pos;
+        var pos = 0;
+        var ratio;
+        for(var i=0;i<arr.length;++i) {
+            sum += arr[i].size;
+            if(Math.abs(sum-weight/2) < splitsum) {
+                pos = i+1;
+                splitsum = Math.abs(sum-weight/2);
+                ratio = sum / weight;
+            }
         }
 
         var arr1 = arr.slice(0, pos);
         var arr2 = arr.slice(pos);
 
-        if(blah) {
-            --blah;
-        if(w>2*h) {
-            positionArray(arr1, x, y, w/2|0,h);
-            positionArray(arr2, x + w/2|0, y, w-(w/2|0),h);
+        if(w>1.2*h) {
+            positionArray(arr1, x, y, w*ratio|0,h);
+            positionArray(arr2, x + w*ratio|0, y, w-(w*ratio|0),h);
         } else {
-            positionArray(arr1, x, y, w, h/2|0);
-            positionArray(arr2, x ,y+h/2|0, w, h-(h/2|0));
-        }
+            positionArray(arr1, x, y, w, h*ratio|0);
+            positionArray(arr2, x ,y+h*ratio|0, w, h-(h*ratio|0));
         }
     }
 
     $(function() {
+        $('body').css('overflow', 'hidden');
+        $('body').css('background', 'black');
         $('a').css('border', 'none');
         var menu = elemToObj($('div > ul > li')[0]);
  //       $('div > ul > li')[1].style.display = 'none';
         reset(menu);
         calcSize(menu);
-        position(menu, 0,0,$(window).width(), $(window).height());
-        console.log(menu);
+        position(menu, 0-margin,0-margin,$(window).width()+margin, $(window).height()+margin);
+
+        function niceSingle(fn) {
+            var running = false;
+            return function() {
+                if(running) {
+                    return;
+                }
+                running = true;
+                setTimeout(function() {
+                    fn();
+                    running = false;
+                }, 100);
+            }
+        }
+
+        $(window).resize(niceSingle(function() {
+            position(menu, 0-margin,0-margin,$(window).width()+margin, $(window).height()+margin);
+        }));
     });
 });
