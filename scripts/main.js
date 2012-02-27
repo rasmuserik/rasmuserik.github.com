7eb2c84c639088c13babb3fbd648eb6e1fbff684
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2012-02-27 20:09:25 +0100

    cleanup

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,33 @@
-document.write('blah');
-define('modernizr', function() { return Modernizr; });
-define('window', function() { return window; });
-define('zquery', function() { return $; });
-require.config({baseUrl:"/scripts"})
-require(['menu']);
+define(['menu'], function() {
+/*
+['solsort.dk',
+    {title: 'Curriculum Vitae', app: showHTML('/CurriculumVitae')},
+    ['Apps',
+        {title: 'Danske Byer', app: redirect('/dkcities')},
+        {title: 'NoteScore', app: redirect('/notescore')},
+        {title: 'PlanetCute', app: redirect('/planetcute')}],
+    ['Notes',
+        {title: 'Tekststruktur for rapporter', app: showMarkDown('/notes/tekststruktur-for-rapporter')},
+        {title: 'Tommelfingerregler for skrivning', app: showMarkDown('/notes/tommelfingerregler-for-skrivning')},
+        {title: 'Fototips', app: showMarkDown('/notes/fototips')},
+        {title: 'TED talks', app: showMarkDown('/notes/ted')},
+        ['Tech',
+            {title: 'Cloud9 and Ender', app: showMarkDown('/notes/tech/c9ender')},
+            {title: 'Jekyll / github-pages', app: showMarkDown('/notes/tech/jekyll')},
+            {title: 'vim', app: showMarkDown('/notes/tech/vim')},
+            {title: 'CoffeeScript', app: showMarkDown('/notes/tech/coffee')}]],
+    ['JavaScript',
+        {title: 'devintro', app: showMarkDown('/devintro/book')},
+        ['JS1K',
+            {title: '4djuliabrot', app: showMarkDown('/f1k/blah')},
+            {title: 'rain', app: showMarkDown('/f1k/rain')}],
+        ['Tweet-size',
+            {title: 'brownian motion', app: showMarkDown('/f1k/brown')},
+            {title: 'sierpinsky', app: showMarkDown('/f1k/sierpinsky')}],
+       {title: 'iOS style', app: showMarkDown('')},
+       {title: 'hierachical layout', app: showMarkDown('')},
+       {title: 'slidein transitions', app: showMarkDown('')}],
+    ['Presentations',
+        {title: 'OAuth2', app: showMarkDown('/presentations/oauth2')}]];
+*/
+});
